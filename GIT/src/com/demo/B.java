196b960076b7c566abd1fe891e6e52ed397a98c8
HP <HP@192.168.100.14>
2023-03-01 09:00:35 +0530

    by vadhfsg

---
+++ GIT/src/com/demo/B.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class B {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello ---------");
+		
+	}
+	public void m1()
+	{
+	System.out.println("Hello ---------");
+	
+	}
+}
