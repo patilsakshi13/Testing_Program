a0862ed02613651fa7d315adb3bb5ae2246cee7d
HP <HP@192.168.100.10>
2023-03-02 13:44:30 +0530

    By smita

--- Jenkins/src/Demo.java
+++ Jenkins/src/Demo.java
@@ -4,5 +4,14 @@ public class Demo
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
+		System.out.println("Hi");
+		System.out.println("Ho"); 
+		
+	}
+	public void m1()
+	{
+		System.out.println("vdfgdhgcg");
+		
+		
 	}
 }
